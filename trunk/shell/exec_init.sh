051d1f622867ca598bd97b7fe547bf67c95f93d4
jun384668960 <Donyj_ljj@163.com>
2018-07-31 22:33:07 +0800

    新库

--- trunk/shell/exec_init.sh
+++ trunk/shell/exec_init.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+ifconfig eth0 up
+ifconfig eth0 192.168.1.117
+
 echo "ismod btn_drv.ko"
 cd /ko
 insmod btn_drv.ko
 insmod led_drv.ko
+insmod wm_8960.ko
 
 echo "start hotplug listen"
 cd /var/shell
